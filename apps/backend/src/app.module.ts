60c9d9705c3518d1e1c2c4f80a66178fc7608897
Fera <feraferaca@gmail.com>
2026-05-04 22:19:14 +0300

    feat: create backend structure

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoomsModule } from './rooms/rooms.module';
+import { SpotifyModule } from './spotify/spotify.module';
 
 @Module({
-  imports: [],
+  imports: [RoomsModule, SpotifyModule],
   controllers: [AppController],
   providers: [AppService],
 })
